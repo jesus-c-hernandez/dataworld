844aa86532c84ca42738701436c8cc29c5715106
Jesus Saul Hernandez <jesus.castaneda@metricamovil.com>
2021-10-04 02:56:16 -0500

    Servicio de geolocalizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GeolocationService } from './core/services/geolocation/geolocation.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dataworld';
+
+  latitude: number;
+  longitude: number;
+
+  constructor(
+    private geolocationService: GeolocationService 
+  ){
+    this.getLocation();
+  }
+
+  getLocation() {
+    this.geolocationService.getPosition().then(pos => {
+        this.latitude = pos.lat;
+        this.longitude = pos.lng;
+        console.log('POS', pos);
+    }).catch( pos => {
+      this.latitude = pos.lat;
+      this.longitude = pos.lng;
+      console.log('POS ERR', pos);
+    })
+}
 }
